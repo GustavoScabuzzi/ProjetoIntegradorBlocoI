b9ab249acc3042ae2dd577ae997a6c3bce500331
GScabuzzi <gustavo-gsg@hotmail.com>
2021-09-25 00:07:35 -0300

    Blog Pessoal - First Commit

---
+++ blogGScabuzzi/src/main/java/org/generation/blogGScabuzzi/BlogGScabuzziApplication.java
@@ -0,0 +1,13 @@
+package org.generation.blogGScabuzzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogGScabuzziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogGScabuzziApplication.class, args);
+	}
+
+}
---
+++ blogGScabuzzi/src/test/java/org/generation/blogGScabuzzi/BlogGScabuzziApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.blogGScabuzzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogGScabuzziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
